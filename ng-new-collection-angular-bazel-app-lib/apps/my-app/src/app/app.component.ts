2b4c4ae8271af0e988afb495a034bc8f5bb56f0c
Conal RYAN <conal.ryan@amadeus.com>
2019-08-20 10:14:13 -0400

    Success load apps/my-app structure

--- ng-new-collection-angular-bazel-app-lib/apps/my-app/src/app/app.component.ts
+++ ng-new-collection-angular-bazel-app-lib/apps/my-app/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-new-collection-angular-bazel-app-lib';
+
+  getTitle = (): string => {
+    return this.title;
+  }
+
 }
