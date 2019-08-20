f0a2216ba2a44bb6659f4ee3dd6ba2c6f4b1110f
Conal RYAN <conal.ryan@amadeus.com>
2019-08-20 12:08:24 -0400

    Lib component inside app component

--- ng-new-collection-angular-bazel-app-lib/apps/my-app/src/app/app.module.ts
+++ ng-new-collection-angular-bazel-app-lib/apps/my-app/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MyLibModule } from '../../../../libs/my-lib/my-lib.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ng-new-collection-angular-bazel-app-lib/libs/my-lib/my-lib.service.ts
+++ ng-new-collection-angular-bazel-app-lib/libs/my-lib/my-lib.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class MyLibService {
 
   constructor() { }
+
+  getLibServiceMessage = (): string => {
+     return 'hello from MyLibService';
+  }
 }
