9495331aa736c968c1380ae5328c6621b6fe0dd0
Conal RYAN <conal.ryan@amadeus.com>
2019-08-20 12:13:48 -0400

    Inject lib service into app component

--- ng-new-collection-angular-bazel-app-lib/apps/my-app/src/app/app.component.ts
+++ ng-new-collection-angular-bazel-app-lib/apps/my-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { MyLibService } from '../../../../libs/my-lib/my-lib.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +10,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ng-new-collection-angular-bazel-app-lib';
 
+  constructor(private readonly _myLibService: MyLibService) {
+
+  }
+
   getTitle = (): string => {
     return this.title;
   }
 
+  getLibServiceMsg(): string {
+    return this._myLibService.getLibServiceMessage();
+  }
 }
